fde90d8c3402d3ac63c5c8c0c38a29e739453195
Fadhuweb <f.abdulazee@alustudent.com>
2024-09-05 06:39:05 -0700

    task

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-"""Unit tests for State class"""
-
-import unittest
-from tests.test_models.test_base_model import TestBaseModel
+""" """
+from tests.test_models.test_base_model import test_basemodel
 from models.state import State
 
 
-class TestState(TestBaseModel):
-    """Test State class"""
+class test_state(test_basemodel):
+    """ """
 
-    def test_name_property(self):
-        """Test the 'name' property of State"""
-        new_state = State()
-        new_state.name = "Arizona"
-        self.assertEqual(type(new_state.name), str)
+    def __init__(self, *args, **kwargs):
+        """ """
+        super().__init__(*args, **kwargs)
+        self.name = "State"
+        self.value = State
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_name3(self):
+        """ """
+        new = self.value(name="test")
+        self.assertEqual(type(new.name), str)
