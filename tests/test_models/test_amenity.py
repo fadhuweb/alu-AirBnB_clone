e19aa1d55ad0e700aacb4973eeaeafb68279e998
Fadhuweb <f.abdulazee@alustudent.com>
2024-09-05 06:33:38 -0700

    task

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,26 +1,19 @@
 #!/usr/bin/python3
-"""Unit tests for Amenity class"""
-
-import unittest
-from tests.test_models.test_base_model import TestBaseModel
+""" """
+from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
 
 
-class TestAmenity(TestBaseModel):
-    """Test Amenity class"""
+class test_Amenity(test_basemodel):
+    """ """
 
     def __init__(self, *args, **kwargs):
-        """Initialize Amenity test instance"""
+        """ """
         super().__init__(*args, **kwargs)
         self.name = "Amenity"
         self.value = Amenity
 
-    def test_name(self):
-        """Test the 'name' attribute of Amenity"""
-        new_amenity = self.value()
-        new_amenity.name = "amenity"
-        self.assertEqual(type(new_amenity.name), str)
-
-
-if __name__ == '__main__':
-    unittest.main()
+    def test_name2(self):
+        """ """
+        new = self.value(name="hbo")
+        self.assertEqual(type(new.name), str)
